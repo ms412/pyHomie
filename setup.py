54a6ccbd2d40bc719942273f0c38730dcad5fc88
Markus Schiesser <markus.schiesser@swisscom.com>
2024-05-03 23:24:51 +0200

    version 0.0.2

--- setup.py
+++ setup.py
@@ -1,8 +1,31 @@
-#!/usr/bin/env python
-
-from setuptools import setup
-
+from distutils.core import setup
 setup(
-    setup_requires=['pbr'],
-    pbr=True,
+  name = 'pyHomie',
+  packages = ['pyHomie'],
+  version = '0.0.2',
+  license='MIT',
+  description = 'A Python library for Homie Convention',
+  author = 'Markus Schiesse',
+  author_email = 'M.Schiesser@gmail.com',
+  url = 'https://github.com/ms412/pyHomie',
+  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',
+  keywords = ['mqtt', 'paho-mqtt', 'homie'],   # Keywords that define your package best
+  install_requires=[            # I get to this in a second
+          'validators',
+          'beautifulsoup4',
+      ],
+  classifier =[
+    'Development Status :: 3 - Alpha',
+    'Environment :: Other Environment',
+    'Intended Audience :: Developers',
+    'Intended Audience :: Information Technology',
+    'License :: Freeware',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.12',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+    'Topic :: Home Automation',
+    ],
+
 )
--- src/pyHomie/__init__.py
+++ src/pyHomie/__init__.py
@@ -1,9 +1,3 @@
-#__name__ = "pyHomie"
-__app__ = "pyHomnie"
-__version__ = "0.0.2"
-__date__= "2021/04/25"
-__author__ = "Markus Schiesser"
-__contact__ = "M.Schiesser@gmail.com"
-__copyright__ = "Copyright (C) 2023 Markus Schiesser"
-__license__ = 'THE BEER-WARE LICENSE'
+from pyHomie import pyHomie
+
 
